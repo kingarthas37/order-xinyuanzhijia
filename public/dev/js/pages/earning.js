86d6a80fb6a6c5925513a8e2e3f8e09c9bfb8f2e
ralf <kingarthas37@hotmail.com>
2016-01-04 16:37:38 +0800

    1.4

--- public/dev/js/pages/earning.js
+++ public/dev/js/pages/earning.js
@@ -26,12 +26,12 @@ module.exports = {
         $('#month-income').text(incomes);
         $('#month-earning').text(incomes - expenseses);
         
-
     },
     editFun:function() {
 
         $('#form-edit-earning').validate();
         var expenses = $('#expenses');
+        var expensesComment = $('#expenses-comment');
         var date = /date=(.*)/.exec(location.search)[1];
         
         $('.current-day-expenses').click(function() {
@@ -42,8 +42,27 @@ module.exports = {
                 },
                 success:function(data) {
                     expenses.val(data.value);
+                    expensesComment.val('采购金额:' + data.value);
                 }
             });
         });
+        
+        $('#expenses-comment,#income-comment').change(function() {
+            
+            var amount = 0;
+            var input = $(this).parent().prev().prev().find('input[type=text]');
+            var amounts = this.value.match(/(\d+).*\b/g);
+            
+            if(!amounts) {
+                input.val(0);
+                return;
+            }
+            
+            for(var i=0;i<amounts.length;i++) {
+                amount += parseInt(amounts[i]);
+            }
+            input.val(amount);
+        });
+        
     }
 };
