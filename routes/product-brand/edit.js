55a00d92226d24104ce90aa0867fd58f22c1b9a1
ralf <kingarthas37@hotmail.com>
2016-02-22 12:32:26 +0800

    2.22

--- routes/product-brand/edit.js
+++ routes/product-brand/edit.js
@@ -11,64 +11,64 @@ var async = require('async');
 var extend = require("xtend");
 
 //class
-var ProductCategory = AV.Object.extend('ProductCategory');
+var ProductBrand = AV.Object.extend('ProductBrand');
 
 var data =  extend(config.data,{
-    title:'产品分类-编辑产品分类',
-    currentPage:'productCategory'
+    title:'产品品牌-编辑产品品牌',
+    currentPage:'productBrand'
 });
 
 
 //编辑产品页
-router.get('/:productCategoryId', function (req, res, next) {
+router.get('/:productBrandId', function (req, res, next) {
 
     if(!req.AV.user) {
         return res.redirect('/login?return=' + encodeURIComponent(req.originalUrl));
     }
     
-    var productCategoryId = parseInt(req.params.productCategoryId);
+    var productBrandId = parseInt(req.params.productBrandId);
 
     data = extend(data,{
         flash: { success:req.flash('success'), error:req.flash('error') },
         user:req.AV.user,
-        productCategoryId: productCategoryId
+        productBrandId: productBrandId
     });
 
-    var query = new AV.Query(ProductCategory);
-    query.equalTo('categoryId', productCategoryId);
+    var query = new AV.Query(ProductBrand);
+    query.equalTo('productBrandId', productBrandId);
     query.first().then(function(result) {
         data = extend(data, {
-            productCategory: result
+            productBrand: result
         });
-        res.render('product-category/edit', data);
+        res.render('product-brand/edit', data);
     });
 
 });
 
 
 
-router.post('/', function (req, res, next) {
+router.post('/:productBrandId', function (req, res, next) {
 
     if(!req.AV.user) {
         return res.redirect('/login?return=' + encodeURIComponent(req.originalUrl));
     }
     
-    var productCategoryId = parseInt(req.body['product-category-id']);
+    var productBrandId = parseInt(req.body['product-brand-id']);
 
-    var categoryName = req.body['name'];
+    var brandName = req.body['name'];
 
-    var query = new AV.Query(ProductCategory);
+    var query = new AV.Query(ProductBrand);
 
-    query.equalTo('categoryId',productCategoryId);
+    query.equalTo('brandId',productBrandId);
     query.first().then(function(result) {
         
-        result.set('categoryName',categoryName);
+        result.set('brandName',brandName);
         
         return result.save();
         
     }).then(function() {
         req.flash('success', '编辑产品分类成功!');
-        res.redirect('/product-category');
+        res.redirect('/product-brand');
     });
 
 });
