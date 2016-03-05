caafd50d616f5ec1eb2e27079c8efd7ad16b01b0
ralf <kingarthas37@hotmail.com>
2016-03-06 00:01:56 +0800

    3.6

--- public/dev/js/pages/index.js
+++ public/dev/js/pages/index.js
@@ -2,30 +2,72 @@
 
 module.exports = {
 
-    indexFun:function() {
-
-        var myChart = echarts.init($('#chat-earning')[0]);
-
-        var option = {
+    indexFun:function(dataEarning) {
+        
+        var chartEarning = echarts.init($('#chart-earning')[0]);
+        chartEarning.setOption({
             title: {
-                text: 'ECharts 入门示例'
+                text: '2016年收入明细'
             },
-            tooltip: {},
+            tooltip : {
+                trigger: 'axis'
+            },
             legend: {
-                data:['销量']
+                selected:{
+                    '支出金额':true,
+                    '收入金额':true,
+                    '盈利':false
+                },
+                data:['支出金额','收入金额','盈利']
             },
-            xAxis: {
-                data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+            toolbox: {
+                feature: {
+                    saveAsImage: {}
+                }
             },
-            yAxis: {},
-            series: [{
-                name: '销量',
-                type: 'bar',
-                data: [5, 20, 36, 10, 10, 20]
-            }]
-        };
-
-        myChart.setOption(option);
+            grid: {
+                left: '0%',
+                right: '0%',
+                bottom: '0%',
+                containLabel: true
+            },
+            xAxis : [
+                {
+                    show:false,
+                    type : 'category',
+                    boundaryGap : false,
+                    data :dataEarning.date
+                }
+            ],
+            yAxis : [
+                {
+                    type : 'value'
+                }
+            ],
+            series : [
+                {
+                    name:'支出金额',
+                    type:'line',
+                    areaStyle: {normal: {}},
+                    data:dataEarning.expenses,
+                    itemStyle:{normal:{color: '#0e90d2'}}
+                },
+                {
+                    name:'收入金额',
+                    type:'line',
+                    areaStyle: {normal: {}},
+                    data:dataEarning.income,
+                    itemStyle:{normal:{color: '#5eb95e'}}
+                },
+                {
+                    name:'盈利',
+                    type:'line',
+                    areaStyle: {normal: {}},
+                    data:dataEarning.earning,
+                    itemStyle:{normal:{color: '#dd514c'}}
+                }
+            ]
+        });
 
     }
 
--- routes/index/index.js
+++ routes/index/index.js
@@ -4,11 +4,13 @@ var router = require('express').Router();
 var AV = require('leanengine');
 
 var flash = require('connect-flash');
-
-var Earning = AV.Object.extend('Earning');
 var extend = require("xtend");
 
 var config = require('../../lib/config');
+
+var Earning = AV.Object.extend('Earning');
+var OrderTrack = AV.Object.extend('OrderTrack');
+var Customer = AV.Object.extend('Customer');
 
 var data = {
     title: '首页',
@@ -25,27 +27,33 @@ router.get('/',(req,res)=> {
         user:req.AV.user
     });
 
-
-
+    
     AV.Promise.when(
-        new AV.Promise((resolve) => {
+        
+        //月收入统计=
+        new AV.Promise(resolve => {
+
+            let currentDate = new Date();
+            let currentYear = currentDate.getFullYear();
             
             let query = new AV.Query(Earning);
-            
-            query.select('expenses','income');
+            query.greaterThan('date',new Date(currentYear,0,0));
+            query.select('expenses','income','date');
             
             query.find().done(items=> {
 
                 let earning = {
                     expenses:[],
                     income:[],
-                    earning:[]
+                    earning:[],
+                    date:[]
                 };
 
                 items.forEach(item => {
                     earning.expenses.push(item.get('expenses'));
                     earning.income.push(item.get('income'));
                     earning.earning.push(item.get('income') - item.get('expenses'));
+                    earning.date.push( (item.get('date').getMonth() + 1) + '/' + item.get('date').getDate() );
                 });
                 
                 data = extend(data,{
@@ -56,7 +64,9 @@ router.get('/',(req,res)=> {
             });
             
         }),
-        new AV.Promise((resolve) => {
+        
+        //收入统计chart
+        new AV.Promise(resolve => {
             
             let currentDate = new Date();
             let currentYear = currentDate.getFullYear();
@@ -78,7 +88,7 @@ router.get('/',(req,res)=> {
                 items.forEach(item => {
                     earning.expenses += item.get('expenses');
                     earning.income += item.get('income');
-                    earning.earning += item.get('income') + item.get('expenses');
+                    earning.earning += item.get('income') - item.get('expenses');
                 });
                 
                 data = extend(data,{
@@ -88,7 +98,53 @@ router.get('/',(req,res)=> {
                 resolve();
                 
             });
+        }),
+        
+        //发货订单列表
+        new AV.Promise(resolve => {
+
+            var page = 1;
+            var limit = 6;
+            
+            let query = new AV.Query(OrderTrack);
+
+            query.skip((page - 1) * limit);
+            query.limit(limit);
+            query.descending('orderId');
+
+            query.find().then(items => {
+
+                data = extend(data, {
+                    order: items
+                });
+
+                let customerQuery = new AV.Query(Customer);
+
+                //数组查询,用于关联列表查询
+                customerQuery.containedIn('customerId',items.map(x=>x.get('customerId')));
+                return customerQuery.find();
+
+            }).then(results => {
+
+                //对关联查询结果进行重新排序
+                let _results = data.order.map((item)=>{
+                    for(let i=0;i< results.length;i++) {
+                        if(item.get('customerId') === results[i].get('customerId')) {
+                            return results[i];
+                        }
+                    }
+                });
+
+                data = extend(data,{
+                    customer:_results
+                });
+                
+                resolve();
+                
+            });
+            
         })
+        
     ).then(()=> res.render('index', data));
       
     
