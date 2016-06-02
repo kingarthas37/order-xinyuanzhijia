7ffdc0fdb6695fe74ffd89a085dabdfa9c0991c6
ralf <kingarthas37@hotmail.com>
2016-06-02 17:23:18 +0800

    6.2-1

--- routes/purchase-contact/add.js
+++ routes/purchase-contact/add.js
@@ -72,7 +72,7 @@ router.post('/', function (req, res, next) {
         success: function () {
             req.flash('success', '添加联系方式成功!');
             let site = siteType === 'etsy' ? '?site-type=etsy' : '';
-            res.redirect(`/purchase-contact${site}`);
+          //  res.redirect(`/purchase-contact${site}`);
         },
         error: function (err) {
             next(err);
