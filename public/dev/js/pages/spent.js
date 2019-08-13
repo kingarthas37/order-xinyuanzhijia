c26cd2c535efda5498162704c9411bd3686b5a61
Ralf Wang <ralf@extrabux.com>
2019-08-13 12:34:39 +0800

    git

--- public/dev/js/pages/spent.js
+++ public/dev/js/pages/spent.js
@@ -23,15 +23,21 @@ module.exports = {
             totalUser3 += Number($(n).text());
         });
 
+        let totalUser4 = 0;
+        $('.cell-user4').each(function (i,n) {
+            totalUser4 += Number($(n).text());
+        });
+
         $('#total-user1').text(totalUser1.toFixed(2));
         $('#total-user2').text(totalUser2.toFixed(2));
         $('#total-user3').text(totalUser3.toFixed(2));
-        $('#total-spent').text((totalUser1 - totalUser3).toFixed(2));
+        $('#total-user4').text(totalUser4.toFixed(2));
+        $('#total-spent').text((totalUser1 - totalUser3 - totalUser4).toFixed(2));
     },
     editFun:function() {
 
         $('#form-edit-earning').validate();
-        $('#spent-user1-comment,#spent-user2-comment,#spent-user3-comment').change(function() {
+        $('#spent-user1-comment,#spent-user2-comment,#spent-user3-comment,#spent-user4-comment').change(function() {
             
             let amount = 0;
             let input = $(this).parent().prev().prev().find('input[type=text]');
